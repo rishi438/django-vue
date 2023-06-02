91cd405d5d2a536371c900f451b510e7f7211f32
xenn <xenn@pop-os.localdomain>
2023-06-03 02:55:07 +0530

    vue tailwind config

---
+++ Social-network/src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import axios from 'axios' 
+
+axios.defaults.baseURL='https://127.0.0.1:8000'
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router,axios)
+
+app.mount('#app')
