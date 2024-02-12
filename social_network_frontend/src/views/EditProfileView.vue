2b4df4ba403cfe8d74355bfdddd31ad03fd1383c
xenn <rishi734438@gmail.com>
2024-02-13 02:30:58 +0530

    rename feature backend added

--- social_network_frontend/src/views/EditProfileView.vue
+++ social_network_frontend/src/views/EditProfileView.vue
@@ -46,46 +46,49 @@
                     <div class="mt-2 text-stone-700">
                         <label>Current Password</label><br />
                         <Field
-                            name="curr_password"
+                            name="current_password"
                             class="mt-1"
                             placeholder="Your current password"
                             :class="[
                                 'w-full py-4 px-6 border rounded-lg',
-                                !errors.curr_password ? 'border-gray-200' : 'border-red-600'
+                                !errors.current_password ? 'border-gray-200' : 'border-red-600'
                             ]"
                         />
-                        <span name="curr_password" class="ml-2 text-red-600 text-xs line-clamp-6">
-                            {{ errors.curr_password }}</span
+                        <span
+                            name="current_password"
+                            class="ml-2 text-red-600 text-xs line-clamp-6"
+                        >
+                            {{ errors.current_password }}</span
                         >
                     </div>
                     <div class="mt-2 text-stone-700">
                         <label>New Password</label><br />
                         <Field
-                            name="password"
+                            name="password1"
                             class="mt-1"
-                            placeholder="Your password"
+                            placeholder="Your new password"
                             :class="[
                                 'w-full py-4 px-6 border rounded-lg',
-                                !errors.password ? 'border-gray-200' : 'border-red-600'
+                                !errors.password1 ? 'border-gray-200' : 'border-red-600'
                             ]"
                         />
-                        <span name="password" class="ml-2 text-red-600 text-xs line-clamp-6">
-                            {{ errors.password }}</span
+                        <span name="password1" class="ml-2 text-red-600 text-xs line-clamp-6">
+                            {{ errors.password1 }}</span
                         >
                     </div>
                     <div class="mt-2 text-stone-700">
-                        <label>Re-enter Password</label><br />
+                        <label>Confirm Password</label><br />
                         <Field
-                            name="confirm_password"
+                            name="password2"
                             class="mt-1"
-                            placeholder="Re-enter new password"
+                            placeholder="Confirm new password"
                             :class="[
                                 'w-full py-4 px-6 border rounded-lg',
-                                !errors.confirm_password ? 'border-gray-200' : 'border-red-600'
+                                !errors.password2 ? 'border-gray-200' : 'border-red-600'
                             ]"
                         />
-                        <span name="confirm_password" class="ml-2 text-red-600 text-xs">{{
-                            errors.confirm_password
+                        <span name="password2" class="ml-2 text-red-600 text-xs">{{
+                            errors.password2
                         }}</span>
                     </div>
                     <div>
@@ -97,27 +100,27 @@
     </div>
 </template>
 <script>
-import axios from 'axios';
-import { useToastStore } from '../stores/toast';
-import { useUserStore } from '../stores/user';
-import { Field, Form } from 'vee-validate';
+import axios from 'axios'
+import { useToastStore } from '../stores/toast'
+import { useUserStore } from '../stores/user'
+import { Field, Form } from 'vee-validate'
 
 export default (await import('vue')).defineComponent({
     setup() {
-        const toastStore = useToastStore();
-        const userStore = useUserStore();
+        const toastStore = useToastStore()
+        const userStore = useUserStore()
         const schema = {
             email: { required: true, email: userStore.user.email },
             name: 'required',
-            curr_password: 'required|minLength:8',
-            password: 'required|minLength:8',
-            confirm_password: 'required|minLength:8'
-        };
+            current_password: 'required|minLength:8',
+            password1: 'required|minLength:8',
+            password2: 'required|minLength:8'
+        }
         return {
             schema,
             toastStore,
             userStore
-        };
+        }
     },
     components: {
         Field,
@@ -127,33 +130,28 @@ export default (await import('vue')).defineComponent({
         return {
             form: this.schema,
             errors: ''
-        };
+        }
     },
     methods: {
-        submit_form() {
-            if (this.errors !== '') {
-                axios
-                    .post('/api/profile/edit/', this.form)
-                    .then((response) => {
-                        if (response.data.msg == 'User created successfully') {
-                            this.toastStore.show_toast(
-                                5000,
-                                `${response.data.msg}. Please log in`,
-                                'bg-emerald-500'
-                            );
-                            // this.form.email = '';
-                            // this.form.name = '';
-                            // this.form.password = '';
-                            // this.form.confirm_password = '';
-                        } else {
-                            this.toastStore.show_toast(5000, response.data.msg, 'bg-red-300');
-                        }
-                    })
-                    .catch((error) => {
-                        console.log('error', error);
-                    })
-            }
+        submit_form(vals) {
+            console.log(vals)
+            axios
+                .post(`/api/profile/${this.$route.params.id}/edit/`, vals)
+                .then((response) => {
+                    if (response.data.msg == 'User created successfully') {
+                        this.toastStore.show_toast(
+                            5000,
+                            `${response.data.msg}. Please log in`,
+                            'bg-emerald-500'
+                        )
+                    } else {
+                        this.toastStore.show_toast(5000, response.data.msg, 'bg-red-300')
+                    }
+                })
+                .catch((error) => {
+                    console.log('error', error)
+                })
         }
     }
-});
+})
 </script>
